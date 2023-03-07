563e0382d9a3b331a6d1f69dc7de8b44a467fc25
Juan Alberto Virrey Cerriteño <av@iseeyoutech.com>
2023-03-07 13:23:20 -0800

    compiler

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,44 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import path from 'node:path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/main.jsx'),
-      name: 'ISY Widget Support',
-      // the proper extensions will be added
-      fileName: 'index',
+      entry: path.resolve(__dirname, 'src/main.jsx'),
+      name: 'isy-widget-support',
+      formats: ['es', 'umd'],
+      fileName: (format) => `wid.${format}.js`,
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['react'],
+      external: ['react', 'react-dom', 'styled-components'],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
         globals: {
-          // vue: 'react',
+          react: 'React',
+          'react-dom': 'ReactDOM',
         },
       },
     },
+    // lib: {
+    //   // Could also be a dictionary or array of multiple entry points
+    //   entry: resolve(__dirname, 'src/main.jsx'),
+    //   name: 'ISY Widget Support',
+    //   // the proper extensions will be added
+    //   fileName: 'index',
+    // },
+    // rollupOptions: {
+    //   // make sure to externalize deps that shouldn't be bundled
+    //   // into your library
+    //   external: ['react'],
+    //   output: {
+    //     // Provide global variables to use in the UMD build
+    //     // for externalized deps
+    //     globals: {
+    //       // vue: 'react',
+    //     },
+    //   },
+    // },
   },
 })
