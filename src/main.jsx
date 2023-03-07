20e0f3259d85cdb2f0dfc7e670d265a12e995ce3
Juan Alberto Virrey Cerriteño <av@iseeyoutech.com>
2023-03-07 13:31:25 -0800

    Se removió render

--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,19 @@ import App from './App'
 
 
 // /consol
-console.log("e", import.meta.env)
+// console.log("e", import.meta.env)
 
 
-if (import.meta.env.DEV)
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <App
-        email="av@iseeyoutech.com"
-        nombre="Juan Alberto Virrey"
-        proyectoId="632b31b87957e70d98f85999"
-      />
-    </React.StrictMode>,
-  )
+// if (import.meta.env.DEV)
+//   ReactDOM.createRoot(document.getElementById('root')).render(
+//     <React.StrictMode>
+//       <App
+//         email="av@iseeyoutech.com"
+//         nombre="Juan Alberto Virrey"
+//         proyectoId="632b31b87957e70d98f85999"
+//       />
+//     </React.StrictMode>,
+//   )
 
 
 export default App
